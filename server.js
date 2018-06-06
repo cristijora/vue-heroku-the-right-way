59fb7542bdb24b935a0116bb62405b9b13f23ff4
cristij <joracristi@gmail.com>
2018-06-06 10:55:41 +0300

    Fix port inside server.js

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 80;
 app.listen(port);
 
